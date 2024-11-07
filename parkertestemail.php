39b7f9d4aa0ee462ee6ff243469de4fbb26c5039
mengle2332 <englep@etown.com>
2024-11-07 08:15:35 -0500

    continuing mail testing

--- parkertestemail.php
+++ parkertestemail.php
@@ -1,5 +1,8 @@
 <?php
 
-mail("pme0420@gmail.com", "New message test yeah", "Hi hello", "yes");
+
+mail("pmeo420@gmail.com", 'New message from your website', "The Message", "The Headers")) {
+    echo "Thank you! Your message has been sent. Please allow us 3-5 business days to respond.";
+    header("refresh:5;url=index.php"); // Redirect after 5 seconds
 
 ?>
