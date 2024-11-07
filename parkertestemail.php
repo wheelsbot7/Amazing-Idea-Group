b4d2fcb7bca3cb4059d7f157a985c228ea4e77fa
mengle2332 <englep@etown.com>
2024-11-07 10:35:59 -0500

    Testing return to homepage function

--- parkertestemail.php
+++ parkertestemail.php
@@ -1,9 +1,16 @@
 <?php
 
+$to = "pme0420@gmail.com";
+$message = "Sample message";
+$headers = "Sample header";
 
-mail("pme420@gmail.com", 'New message from your website', "The Message", "The Headers") {
+if(mail($to, 'New message from your website', $message, $headers)){
     echo "Thank you! Your message has been sent. Please allow us 3-5 business days to respond.";
-    header("refresh:5;url=index.php"); // Redirect after 5 seconds
+    //header("refresh:5;url=index.php"); // Redirect after 5 seconds
+    }else {
+    echo "Oops! Something went wrong while sending the email.";
+    echo error_get_last()['message'];
 }
+
 
 ?>
