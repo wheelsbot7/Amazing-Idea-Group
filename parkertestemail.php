a61d2fc9f8f0d4e5275599e5347d1b518a8a4373
mengle2332 <englep@etown.com>
2024-11-07 08:16:18 -0500

    Fixing spelling mistake

--- parkertestemail.php
+++ parkertestemail.php
@@ -1,7 +1,7 @@
 <?php
 
 
-mail("pmeo420@gmail.com", 'New message from your website', "The Message", "The Headers")) {
+mail("pme420@gmail.com", 'New message from your website', "The Message", "The Headers")) {
     echo "Thank you! Your message has been sent. Please allow us 3-5 business days to respond.";
     header("refresh:5;url=index.php"); // Redirect after 5 seconds
 
