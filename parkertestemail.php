f95579cb828cc4643ecf914c0804191dfc32d459
mengle2332 <englep@etown.com>
2024-11-07 08:23:06 -0500

    Fixing bracket

--- parkertestemail.php
+++ parkertestemail.php
@@ -4,5 +4,6 @@
 mail("pme420@gmail.com", 'New message from your website', "The Message", "The Headers")) {
     echo "Thank you! Your message has been sent. Please allow us 3-5 business days to respond.";
     header("refresh:5;url=index.php"); // Redirect after 5 seconds
+}
 
 ?>
