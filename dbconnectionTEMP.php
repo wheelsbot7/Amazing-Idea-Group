7a32fe514652faa198b574eae3c0f8d759b2e4be
acs202204 <acs202204@gmail.com>
2024-11-07 09:18:52 -0500

    local config file

--- dbconnectionTEMP.php
+++ dbconnectionTEMP.php
@@ -1,8 +1,8 @@
 <?php  
-    $server = '156.67.74.51';
-    $username = 'u413142534_inspire';
-    $password = 'WeN33dMoreIde@s!';
-    $database = 'u413142534_projects';
+    $server = '127.0.0.1';
+    $username = 'root';
+    $password = '';
+    $database = 'projects';
     $con = mysqli_connect($server, $username, $password, $database);
 
     // Create connection
