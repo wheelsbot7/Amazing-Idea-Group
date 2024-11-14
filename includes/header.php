40b24a59257388c230ba57c4674b603dc8f5ea01
acs202204 <acs202204@gmail.com>
2024-11-14 09:13:31 -0500

    logo

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,9 @@
 <body>
     <div class="w3-top">
         <div class="w3-bar w3-white w3-wide w3-padding w3-card">
-            <a href="#home" class="w3-bar-item w3-button"><b>ECS</b> Projects</a>
+            <a href="index.php#home" target="_blank">
+                <img src="images/Logos/blueJay2BU.png" width="100" height="100">
+            </a>
 
             <div class="w3-right w3-hide-small">
                 <a href="index.php#home" class="w3-bar-item w3-button">Home</a>
