70660c2d8ba1132529c24157ed57804d28f5df84
jxmill <jxm703@ptd.net>
2024-11-21 09:22:17 -0500

    Creating admin.php

---
+++ data_src/api/admin.php
@@ -0,0 +1,50 @@
+<?php
+require "db_config.php";
+session_start();
+
+$conn = new mysqli($host, $dbUsername, $dbPassword, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: ".$conn->connect_error);
+}
+
+if (!isset($_POST['username'], $_POST['password'])) {
+    exit('Please complete the registration form!');
+}
+
+if ($qry = $conn->prepare("SELECT adminID, password FROM users WHERE username = ?")) {
+    $qry->bind_param("s", $_POST["username"]);
+    $qry->execute();
+    $qry->store_result();
+
+    if ($qry->num_rows > 0) {
+        $qry->bind_result($id, $password);
+        $qry->fetch();
+        
+        if (password_verify($_POST["password"], $password)) {
+            session_regenerate_id();
+            $_SESSION["loggedin"] = TRUE;
+            $_SESSION["name"] = $_POST["username"];
+            $_SESSION["id"] = $id;
+            
+            header("Location: ../../../Amazing-Idea-Group/tempTable.php");
+        }
+        else {
+            header("Location: ../../../Amazing-Idea-Group/login.php");
+        }
+    } else {
+        echo "Incorrect username, try again.";
+        header("Location: ../../../Amazing-Idea-Group/login.php");
+    }
+    $qry->close();
+    
+
+} else {
+    echo "Incorrect username or password, try again.";
+    header("Location: ../../../Amazing-Idea-Group/login.php");
+}
+
+$conn->close();
+
+exit();
+?>
