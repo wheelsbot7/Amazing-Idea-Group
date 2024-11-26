b7bddb9fcc6c278cff9f234b33efa9a63c929db7
jxmill <jxm703@ptd.net>
2024-11-26 10:14:42 -0500

    Showing all tables in website data sql file

--- data_src/Website Data.sql
+++ data_src/Website Data.sql
@@ -1,4 +1,8 @@
 USE u413142534_projects;
 SHOW Tables;
-SELECT * FROM projects;
 SELECT * FROM fields;
+SELECT * FROM projects;
+SELECT * FROM proj_field;
+SELECT * FROM project_notes;
+SELECT * FROM pending;
+SELECT * FROM users;
